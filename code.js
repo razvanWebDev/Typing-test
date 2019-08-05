a3cfeeb9d32d72115c642de7b3aded137ad2215d
Razvan Crisan <crsn_razvan@yahoo.com>
2019-08-05 15:00:25 +0300

    random text on load

--- code.js
+++ code.js
@@ -1,1 +1,4 @@
-eventsModule.init(1, 1);
+var getText = wordsModule.words().length;
+var randomText = Math.floor(Math.random() * getText);
+console.log(randomText);
+eventsModule.init(60, randomText);
